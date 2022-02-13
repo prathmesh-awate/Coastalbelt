ef08d94963a481eb987ac76a2f9fd4b4f686e4b0
prathmesh-awate <prathm235@gmail.com>
2022-02-13 17:50:32 +0530

    Added header

---
+++ my-app/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header(){
+    return(
+        <header>
+            <nav>
+                <li>Home</li>
+                <li>Pricing</li>
+                <li>About us</li>
+            </nav>
+        </header>
+    )
+}
