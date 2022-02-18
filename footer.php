87feb087120c3789c03a80d8f1d29c415d11c844
nameisvinay <99533615+nameisvinay@users.noreply.github.com>
2022-02-18 23:29:37 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap 5 Website Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  
+ 
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+  <style>
+  .fakeimg {
+    height: 200px;
+    background: #aaa;
+  }
+  ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  }
+  
+  .fa {
+  padding: 20px 1px;
+  font-size: 20px;
+  width: 50px;
+  height: 50px;
+  text-align: left;
+  text-decoration: none;
+  margin: 0px 0px;
+  border-radius: 100%;
+  
+}
+
+.fa:hover {
+    opacity: 0.7;
+}
+
+.fa-facebook {
+ 
+  color: white;
+}
+
+.fa-twitter {
+
+  color: white;
+}
+
+.fa-youtube {
+ 
+  color: white;
+}
+
+.fa-instagram {
+
+  color: white;
+}
+
+.fa-pinterest {
+ 
+  color: white;
+}
+
+}
+  
+  </style>
+</head>
+<body>
+<div class="mt-5 p-4 bg-dark text-white text-LEFT">
+  <p><h2>Coastalbelt</h2></p>
+  <ul>
+  
+   <li>CONTACT NO:</li>
+   <li>ADDRESS:</li>
+  </ul> 
+  
+  
+
+<!-- Add font awesome icons -->
+<a href="#" class="fa fa-facebook"></a>
+<a href="#" class="fa fa-twitter"></a>
+<a href="#" class="fa fa-youtube"></a>
+<a href="#" class="fa fa-instagram"></a>
+<a href="#" class="fa fa-pinterest"></a
+  
+</div>
+
+</body>
+</html>
