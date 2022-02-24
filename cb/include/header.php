0b231fa77198ead101c65a778f40b936ce7fcb66
prathmesh-awate <prathm235@gmail.com>
2022-02-24 23:55:18 +0530

    configured

--- cb/include/header.php
+++ cb/include/header.php
@@ -1,4 +1,23 @@
-<?php include 'boilerplate.php';?>
+<!Doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="./styles/home.css">
+    <title>Hello, world!</title>
+  </head>
+  <body>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    
+
 <nav class="navbar navbar-light navbar-expand-lg shadow-sm p-3 mb-5 bg-white rounded" style="background-color: #ffffff;">
             <div class="container-fluid">
                 <nav class="navbar navbar-light bg-light">
